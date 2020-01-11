4236687d457c5397c4d20a3d53e0458be846a90d
May <themaymeow@gmail.com>
2020-01-11 14:28:47 +0100

    Add xUnit tests for entities

---
+++ MayMeow.Blog.Entities.Tests/NodeTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MayMeow.Blog.Entities.Content;
+using Xunit;
+
+namespace MayMeow.Blog.Entities.Tests
+{
+    public class NodeTest
+    {
+        [Fact]
+        public void TestModel()
+        {
+            var model =  new Node
+            {
+                Id = 1,
+                Title = "Model title",
+                Body = "Some text in body",
+                CreatedAt = DateTime.Now,
+                ModifiedAt = DateTime.Now
+            };
+            
+            Assert.Equal("Model title", model.Title);
+            Assert.Equal("Some text in body", model.Body);
+        }
+    }
+}
